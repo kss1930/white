c85fa33d7f604bf674174636f5a861d6d07a70a6
egreen-park <71594779+egreen-park@users.noreply.github.com>
2020-12-13 12:07:00 +0900

    Update hyundaican.py
    ALLNew K7 2.2

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -45,6 +45,13 @@ def create_lkas11(packer, frame, car_fingerprint, apply_steer, steer_req,
     values["CF_Lkas_LdwsActivemode"] = 2
     values["CF_Lkas_SysWarning"] = lkas11["CF_Lkas_SysWarning"]
 
+  elif car_fingerprint == CAR.K7:
+    values["CF_Lkas_LdwsActivemode"] = 2
+#    values["CF_Lkas_SysWarning"] = lkas11["CF_Lkas_SysWarning"]    
+#    values["CF_Lkas_Bca_R"] = 0
+    values["CF_Lkas_FcwOpt_USM"] = 1
+    values["CF_Lkas_LdwsOpt_USM"] = 3
+
   elif car_fingerprint == CAR.SONATA_LF_TURBO:
     values["CF_Lkas_FcwOpt_USM"] = 2 if enabled else 1
     values["CF_Lkas_LdwsOpt_USM"] = 2
